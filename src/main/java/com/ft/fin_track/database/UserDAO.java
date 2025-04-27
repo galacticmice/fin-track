355a68972b4254d0ba3e76f1e44f9028325799fb
ben <172694952+galacticmice@users.noreply.github.com>
2025-04-26 23:31:14 -0700

    user table DAO

--- src/main/java/com/ft/fin_track/database/UserDAO.java
+++ src/main/java/com/ft/fin_track/database/UserDAO.java
@@ -4,6 +4,11 @@ import java.sql.*;
 
 public class UserDAO {
 
+    /**
+     * add user to database
+     * @param user object to add
+     * @return true if success
+     */
     public static boolean addUser(User user) {
         try (Connection conn = ConnectDB.getConnection()) {
             String query = "INSERT INTO Users (username, password, date_created) VALUES (?, ? ,?)";
@@ -22,8 +27,48 @@ public class UserDAO {
         }
     }
 
-    public static User getUserById(int id) {
-        // Logic to retrieve user from the database by ID
-        return null;
+    /**
+     * delete user from database for given id
+     * @param user_id to delete
+     * @return true if success
+     */
+    public static boolean deleteUser(int user_id) {
+        try (Connection conn = ConnectDB.getConnection()) {
+            String query = "DELETE FROM Users WHERE user_id = ?";
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setInt(1, user_id);
+            ps.executeUpdate();
+
+            ps.close();
+            conn.close();
+            return true;
+        } catch (SQLException e) {
+            System.out.println("Error adding user: " + e.getMessage());
+            return false;
+        }
+    }
+
+    /**
+     * update user in database (limit to username and password)
+     * @param user_id to update
+     * @param updated_user new updated user object
+     * @return true if success
+     */
+    public static boolean updateUser(int user_id, User updated_user) {
+        try (Connection conn = ConnectDB.getConnection()) {
+            String query = "UPDATE Users SET username=?, password=? WHERE user_id=?";
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setString(1, updated_user.getUsername());
+            ps.setString(2, updated_user.getPassword());
+            ps.setInt(3, user_id);
+            ps.executeUpdate();
+
+            ps.close();
+            conn.close();
+            return true;
+        } catch (SQLException e) {
+            System.out.println("Error adding user: " + e.getMessage());
+            return false;
+        }
     }
 }
