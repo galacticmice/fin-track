044ffa06889551b5822e33d400d40dbe969a64c1
ben <172694952+galacticmice@users.noreply.github.com>
2025-05-02 15:35:39 -0700

    connection to supabase-db

--- src/main/java/com/ft/fin_track/FinTrackApplication.java
+++ src/main/java/com/ft/fin_track/FinTrackApplication.java
@@ -2,8 +2,11 @@ package com.ft.fin_track;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class FinTrackApplication {
 
 	public static void main(String[] args) {
